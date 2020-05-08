3422ef9d83e0af1c67edfea8958a5380c3dcdacf
dohyun kim <64508411+DOKKU2714@users.noreply.github.com>
2020-05-08 11:25:41 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,9 +3,13 @@
 int main(void){
 
   int number;
+  char name[10];
   printf("당신의 학번을 입력하세요 :");
   scanf("%d", &number);
+  printf("당신의 이름을 입력하세요 : ");
+  scanf("%s", &name);
   printf("당신의 학번은 %d 입니다.\n", number);
+  printf("당신의 이름은 %s 입니다.\n", name);
 
   return 0;
 }
